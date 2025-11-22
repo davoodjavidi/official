3480a4aab3f0da11e349237862a0f270ba0fc7ca
davood javidi <javididavid1993@gmail.com>
2025-11-22 23:32:47 +0330

    edit nav in dashboard

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -32,7 +32,7 @@ export default function NavLinks() {
             key={link.name}
             href={link.href}
             className={`flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3 ${
-              pathname === link.href ? "text-white bg-blue-800" : ""
+              pathname === link.href ? "text-black font-bold bg-blue-800" : ""
             } `}
           >
             <LinkIcon className="w-6" />
